a792a2cee4b821d8f9d35868b83368894a04fc1a
Chetan Lohkare <55937356+chetan-187@users.noreply.github.com>
2020-10-01 14:31:24 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
 
-void main(){
+int main(){
 
     printf("Hello this is done on local");
     printf("Heyy this is done on the remote");
-
+    
+getch();
+return 0;
 }
